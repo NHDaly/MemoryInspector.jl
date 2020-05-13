cfde0393beac3414b1ea89dbbb1c8f6d4b4980f8
Nathan Daly <nhdaly@gmail.com>
2020-05-13 07:56:37 -0400

    Make tests into actual tests using Test

--- test/MemoryInspector.jl
+++ test/MemoryInspector.jl
@@ -1,5 +1,6 @@
 module MemoryInspectorTest
-import ..MemoryInspector
+import MemoryInspector
+using Test
 
 mutable struct Obj{T1,T2}
     a::T1
@@ -10,21 +11,45 @@ mutable struct Obj{T1,T2}
     Obj(a::A,b::B) where {A,B} = Obj{A,B}(a,b)
 end
 
-s = join(rand('a':'z', 1024*1024)); # 1 MiB string
-multistring = Obj(s,s);   # Shared references to large object
-selfie = Obj{Obj,Obj}(multistring);
-selfie.b = selfie;    # Self-reference
+@testset "self-references" begin
+    s = join(rand('a':'z', 1024*1024)); # 1 MiB string
+    multistring = Obj(s,s);   # Shared references to large object
+    selfie = Obj{Obj,Obj}(multistring);
+    selfie.b = selfie;    # Self-reference
 
-# Not sure how to test an interactive UI like this.
-#MemoryInspector.@inspect selfie
-result = MemoryInspector.MemorySummarySize.summarysize(selfie)
+    # Not sure how to test an interactive UI like this.
+    #MemoryInspector.@inspect selfie
+    result = MemoryInspector.MemorySummarySize.summarysize(selfie)
 
-# Humanize.datasize(Base.summarysize(sh), style=:bin)
-#
-# serialize("/tmp/sh", sh)
-# run(`ls -lh /tmp/sh`)  # Size on disk *IS 2MiB!*
-#
-# sh_deserialized = deserialize("/tmp/sh");  # But somehow the strings are deduplicated when deserialized
-# Humanize.datasize(Base.summarysize(sh), style=:bin)
+    @test result.size == Base.summarysize(selfie)
+    @test length(result.children) == 2
+
+    child_sizes = [v.size for (k,v) in result.children]
+    # Test that all the bytes are accounted for: the multistring child and the self-reference.
+    @test sum(child_sizes) + sizeof(selfie) == result.size  # allocated children + pointers.
+    @test Base.summarysize(selfie.a) in child_sizes
+    @test 0 in child_sizes  # selfie.b is a self-reference so adds no size besides its pointers.
+end
+
+@testset "handles distinct mutable structs with same value" begin
+    double_arr = Obj([1,2], [1,2])
+
+    result = MemoryInspector.MemorySummarySize.summarysize(double_arr)
+    @test result.size == Base.summarysize(double_arr)
+end
+@testset "handles distinct immutable structs with same value" begin
+    double_arr = Obj((1,2), (1,2))
+
+    result = MemoryInspector.MemorySummarySize.summarysize(double_arr)
+    @test result.size == Base.summarysize(double_arr)
+end
+
+@testset "Complex Object" begin
+    result = MemoryInspector.MemorySummarySize.summarysize(Base)
+    # I'm undercounting some things here... I think I'm being over-generous with self-references, 
+    # counting things that have the same _value_ but aren't _actually_ self references? I'm not
+    # sure though.
+    @test_broken result.size == Base.summarysize(Base)
+end
 
 end
